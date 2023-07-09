da88e8ed359c272e92f2c206d5fceea94ebd7085
Darya Skvortsova <335045@niuitmo.ru>
2023-07-09 15:10:39 +0300

    update loader

--- loaders/i18n-loader.cjs
+++ loaders/i18n-loader.cjs
@@ -1,11 +1,8 @@
-const loaderUtils = require("loader-utils");
-
 module.exports = function(source) {
-  const options = loaderUtils.getOptions(this); // getting options from webpack.config 
-
-  Object.keys(options.translations).forEach(key => {
-    source = source.replace(new RegExp(`i18n\\('${key}'\\)`, "g"), `"${options.translations[key]}"`);
-  });
-
-  return source;
-};
+    let newSource = source;
+    for (const [key, value] of Object.entries(this.query.translations)) {
+        const regex = new RegExp(`i18n\\(['"]${key}['"]\\)`, 'g');
+        newSource = newSource.replace(regex, `'${value}'`);
+    }
+    return newSource;
+}

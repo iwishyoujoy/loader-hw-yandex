c64922759c16f7918acde93081f2813ba305e13d
Darya Skvortsova <335045@niuitmo.ru>
2023-07-09 14:44:15 +0300

    add comments

--- loaders/i18n-loader.cjs
+++ loaders/i18n-loader.cjs
@@ -1,7 +1,7 @@
 const loaderUtils = require("loader-utils");
 
 module.exports = function(source) {
-  const options = loaderUtils.getOptions(this);
+  const options = loaderUtils.getOptions(this); // getting options from webpack.config 
 
   Object.keys(options.translations).forEach(key => {
     source = source.replace(new RegExp(`i18n\\('${key}'\\)`, "g"), `"${options.translations[key]}"`);

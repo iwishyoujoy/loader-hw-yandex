6e33a598ccbbc676c0b356fbaa3ff13d5c9a8d9d
Darya Skvortsova <335045@niuitmo.ru>
2023-07-09 15:31:04 +0300

    new regex

--- loaders/i18n-loader.cjs
+++ loaders/i18n-loader.cjs
@@ -1,8 +1,10 @@
-module.exports = function(source) {
+module.exports = function (source) {
     let newSource = source;
+  
     for (const [key, value] of Object.entries(this.query.translations)) {
-        const regex = new RegExp(`i18n\\(['"]${key}['"]\\)`, 'g');
-        newSource = newSource.replace(regex, `'${value}'`);
+      const regex = new RegExp(`i18n\\s*\\(['"]${key}['"]\\)`, "g");
+      newSource = newSource.replace(regex, JSON.stringify(value));
     }
+    
     return newSource;
-}
+  };
